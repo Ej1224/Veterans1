8243661e99bd8998f8184ee076b1729a789c5546
Ej1224 <39943828+Ej1224@users.noreply.github.com>
2018-11-11 14:17:59 +0800

    isa pang commit

--- Veterans1/Form1.Designer.vb
+++ Veterans1/Form1.Designer.vb
@@ -47,7 +47,7 @@ Partial Class Form1
         Me.Button1.Name = "Button1"
         Me.Button1.Size = New System.Drawing.Size(154, 46)
         Me.Button1.TabIndex = 2
-        Me.Button1.Text = "Button1"
+        Me.Button1.Text = "Enter"
         Me.Button1.UseVisualStyleBackColor = True
         '
         'Form1
--- Veterans1/Form1.vb
+++ Veterans1/Form1.vb
@@ -1,6 +1,6 @@
 ﻿Public Class Form1
 
-    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
         MsgBox("Hello World")
     End Sub
 End Class
